9f3909bd5dbbd21c95a992b52c145eb427af595f
alanaandres <alanaandres@aBigMac.local>
2021-06-03 23:58:33 -0700

    added database

---
+++ src/routes/eventsRouter.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const eventsController = require('../controllers/eventsController')
+
+router.get('/', eventsController.getEvents)
+
+// Create new Event 
+router.post('/', eventsController.createEvent)
+
+// Get Event by ID
+router.get('/:id', eventsController.findEvent)
+
+//g et  event by id and update 
+router.put('/:id', eventsController.updateEvent)
+
+//Get specific event by id and delete 
+router.delete('/:id', eventsController.deleteEvent)
+
+module.exports = router
+
